d5df99690ad2007e9d2c7052a615b509bcc57a85
Nadia Ghanizada <149120502+nadiazada@users.noreply.github.com>
2025-11-03 15:03:47 -0800

    Created initial file

---
+++ game.py
@@ -0,0 +1,26 @@
+#python code. Everything can go into one file 
+'''
+Task 1 — Leader setup
+
+Owner: Person 1
+Focus: getting the secret word, asking number of players, and printing rules.
+Implements the setup_game() function.
+
+Task 2 — Player input
+
+Owner: Person 2
+Focus: one player’s turn — prompt and collect their input.
+Implements the player_turn() function.
+
+Task 3 — Guess / question logic
+
+Owner: Person 3
+Focus: detect guesses vs questions, check if correct, and manage responses.
+Implements the handle_input() function.
+
+Task 4 — Main game flow
+
+Owner: Person 4
+Focus: connect all functions together, run rounds, handle win/loss.
+Implements the play_game() function (calls all others)
+'''
